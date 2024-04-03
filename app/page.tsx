374826df4362615f1496275b37f34238b69143d2
ebotsupreme <ejung716@gmail.com>
2024-04-03 11:17:43 -0700

    small text change for testing

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="h-screen flex items-center justify-center text-5xl">
-        TO DO
+        TO DO App
       </div>
     </main>
   );
